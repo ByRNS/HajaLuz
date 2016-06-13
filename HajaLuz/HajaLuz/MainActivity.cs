43a14d39480557959997e037d46d160e39144eba
RonildoSouza <ronildo_rns@hotmail.com>
2016-06-13 14:52:09 -0300

    Ajustes na aplicação

--- HajaLuz/HajaLuz/MainActivity.cs
+++ HajaLuz/HajaLuz/MainActivity.cs
@@ -2,17 +2,22 @@
 using Android.Widget;
 using Android.OS;
 using System;
-using Android.Hardware;
+using AH = Android.Hardware;
+using Android.Content.PM;
+using Android.Graphics;
+using Android.Content.Res;
 
 namespace HajaLuz
 {
-#pragma warning disable CS0618 // Type or member is obsolete
-	[Activity(Label = "Haja Luz!", MainLauncher = true, Icon = "@mipmap/icon")]
+#pragma warning disable CS0618 // Classe Camera e obsoleta
+	[Activity(
+		MainLauncher = true, 
+		ScreenOrientation = ScreenOrientation.Portrait)]
 	public class MainActivity : Activity
 	{
 		ToggleButton tbtInterruptor;
-		Camera camera;
-		Camera.Parameters parametros;
+		AH.Camera camera;
+		AH.Camera.Parameters parametros;
 		
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
@@ -26,26 +31,40 @@ namespace HajaLuz
 
 		public void Interruptor(object sender, EventArgs e)
 		{
-			var t = sender as ToggleButton;
+			var toggleDoSender = sender as ToggleButton;
+			Color corBackground;
+			Color corTexto;
 
-			if (t.Checked)
+			if (toggleDoSender.Checked)
 			{
-				camera = Camera.Open();
+				corTexto = new Color(0, 0, 0);
+				corBackground = new Color(108, 211, 146);
+
+				camera = AH.Camera.Open();
 				parametros = camera.GetParameters();
 				
-				parametros.FlashMode = Camera.Parameters.FlashModeTorch;
+				parametros.FlashMode = AH.Camera.Parameters.FlashModeTorch;
 				camera.SetParameters(parametros);
 				camera.StartPreview();
+
+				toggleDoSender.SetTextColor(corTexto);
+				toggleDoSender.SetBackgroundColor(corBackground);
 			}
 			else
 			{
+				corTexto = new Color(255, 255, 255);
+				corBackground = new Color(0, 0, 0);
+
 				camera.StopPreview();
 				camera.Release();
 				camera = null;
+
+				toggleDoSender.SetTextColor(corTexto);
+				toggleDoSender.SetBackgroundColor(corBackground);
 			}
 		}
 	}
-#pragma warning restore CS0618 // Type or member is obsolete
+#pragma warning restore CS0618 // Classe Camera e obsoleta
 }
 
 
