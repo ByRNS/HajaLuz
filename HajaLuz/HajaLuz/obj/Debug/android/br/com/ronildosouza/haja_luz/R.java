860ee523fb316a11f4fcf1bd12a08a10a078ef46
RonildoSouza <ronildo_rns@hotmail.com>
2016-06-14 15:13:02 -0300

    .

--- HajaLuz/HajaLuz/obj/Debug/android/br/com/ronildosouza/haja_luz/R.java
+++ HajaLuz/HajaLuz/obj/Debug/android/br/com/ronildosouza/haja_luz/R.java
@@ -10,8 +10,13 @@ package br.com.ronildosouza.haja_luz;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int BlackBackground=0x7f040001;
+        public static final int GreenBackground=0x7f040000;
+    }
     public static final class id {
-        public static final int tbtInterruptor=0x7f050000;
+        public static final int tbtInterruptor=0x7f060000;
+        public static final int txvByRns=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -20,7 +25,9 @@ public final class R {
         public static final int haja_luz=0x7f020000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int by_rns=0x7f040001;
+        public static final int app_name=0x7f050000;
+        public static final int by_rns=0x7f050003;
+        public static final int desligado=0x7f050002;
+        public static final int ligado=0x7f050001;
     }
 }
