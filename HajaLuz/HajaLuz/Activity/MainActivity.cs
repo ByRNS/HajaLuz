9681735195c85afbfd407280969197a3d2d410bb
Ronildo N. Souza <ronildo_rns@hotmail.com>
2016-06-22 18:10:22 -0300

    Refatoração do código

---
+++ HajaLuz/HajaLuz/Activity/MainActivity.cs
@@ -0,0 +1,72 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Android.Content.PM;
+using Android.Graphics;
+using Android.Content.Res;
+
+namespace HajaLuz
+{
+#pragma warning disable CS0618 // Classe Camera e obsoleta para API 21 acima
+	[Activity(
+		MainLauncher = true, 
+		ScreenOrientation = ScreenOrientation.Portrait)]
+	public class MainActivity : Activity
+	{
+		Flash flash;
+		readonly Color colorWhite = Color.White;
+		Color colorGreenBackground;
+		Color colorBlackBackground;
+
+		TextView txvByRns;
+		ToggleButton tbtInterruptor;
+		
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.Main);
+
+			colorGreenBackground = Resources.GetColor(Resource.Color.GreenBackground);
+			colorBlackBackground = Resources.GetColor(Resource.Color.BlackBackground);
+
+			flash = new Flash();
+
+			tbtInterruptor = FindViewById<ToggleButton>(Resource.Id.tbtInterruptor);
+			tbtInterruptor.Click += LigaDesliga;
+
+			txvByRns = FindViewById<TextView>(Resource.Id.txvByRns);
+		}
+
+		protected override void OnStop()
+		{
+			base.OnStop();
+
+			tbtInterruptor.Checked = false;
+			tbtInterruptor.CallOnClick();
+		}
+
+		public void LigaDesliga(object sender, EventArgs e)
+		{
+			bool flashLigado = flash.Liga();
+			var corViews = new CorViews();
+
+			var toggleDoSender = (ToggleButton) sender;
+
+			if (toggleDoSender.Checked)
+			{
+				if (flashLigado)
+				{
+					corViews.MudarCor(Color.Black, colorGreenBackground, tbtInterruptor, txvByRns);
+				}
+				else
+					corViews.MudarCor(colorWhite, colorWhite, tbtInterruptor, txvByRns);
+			}
+			else
+			{
+				corViews.MudarCor(colorWhite, colorBlackBackground, tbtInterruptor, txvByRns);
+			}
+		}
+	}
+#pragma warning restore CS0618 // Classe Camera e obsoleta para API 21 acima
+}
---
+++ HajaLuz/HajaLuz/Model/Flash.cs
@@ -0,0 +1,47 @@
+﻿using Android.Content.PM;
+using Android.Hardware;
+
+namespace HajaLuz
+{
+	public class Flash
+	{
+		PackageManager packageManager;
+		Camera camera;
+
+		public bool HasFlash
+		{
+			get
+			{
+				return packageManager.HasSystemFeature(PackageManager.FeatureCameraFlash);
+			}
+		}
+
+		public bool Liga()
+		{
+			if (HasFlash)
+			{
+				camera = Camera.Open();
+				
+				var parametros = camera.GetParameters();
+				parametros.FlashMode = Camera.Parameters.FlashModeTorch;
+				
+				camera.SetParameters(parametros);
+				camera.StartPreview();
+
+				return true;
+			}
+			return false;
+		}
+
+		public void Desliga()
+		{
+			if (camera != null && HasFlash.Equals(true))
+			{
+				camera.StopPreview();
+				camera.Release();
+				camera = null;
+			}
+		}
+	}
+}
+
